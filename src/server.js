eff608e732d920d5508528529c280ed4b5380891
Chaedie <chaedong.im.dev@gmail.com>
2022-06-28 14:01:50 +0900

    feat : Get Method 활용법 실습

--- src/server.js
+++ src/server.js
@@ -3,6 +3,15 @@ import express from 'express';
 const PORT = 4000;
 const app = express();
 
-const handleListening = () => console.log(`Server listening on port "http://localhost:${PORT}"`);
+const handleHome = (req, res) => {
+  return res.send('End the respond Using res.send()');
+};
+const handleLogin = (req, res) => {
+  return res.send('Login here.');
+};
 
+app.get('/', handleHome);
+app.get('/login', handleLogin);
+
+const handleListening = () => console.log(`Server listening on port "http://localhost:${PORT}"`);
 app.listen(PORT, handleListening);
