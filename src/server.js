bf138cffcfffc3caa098d162c84247cac0f97783
Chaedie <chaedong.im.dev@gmail.com>
2022-06-29 16:37:06 +0900

    feat : urlLogger를 구현하며 MiddleWare에 대해 배움

--- src/server.js
+++ src/server.js
@@ -3,15 +3,35 @@ import express from 'express';
 const PORT = 4000;
 const app = express();
 
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === '/protected') {
+    return res.send('<h1>Not Allowed</h1>');
+  }
+  console.log('Allowed, you may continue');
+  next();
+};
+
 const handleHome = (req, res) => {
-  return res.send('End the respond Using res.send()');
+  // next();
+  return res.send('<h1>I love middleWares</h1>');
 };
 const handleLogin = (req, res) => {
   return res.send('Login here.');
 };
+const handleProtected = (req, res) => {
+  return res.send('Welcome to the private lounge.');
+};
 
+app.use(logger);
+app.use(privateMiddleware);
 app.get('/', handleHome);
 app.get('/login', handleLogin);
+app.get('/protected', handleProtected);
 
 const handleListening = () => console.log(`Server listening on port "http://localhost:${PORT}"`);
 app.listen(PORT, handleListening);
