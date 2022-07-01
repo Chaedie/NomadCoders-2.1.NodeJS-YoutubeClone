0ef0f34d86d7149ba8fd40d86a06d47b64fa081c
Chaedie <chaedong.im.dev@gmail.com>
2022-07-01 11:41:22 +0900

    add : 필요한 URL을 정리하여 Route, Controllers 생성

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,5 +1,8 @@
 export const join = (req, res) => res.send('Join');
 export const edit = (req, res) => res.send('Edit');
 export const remove = (req, res) => res.send('Remove');
+export const login = (req, res) => res.send('Login');
+export const logout = (req, res) => res.send('logout');
+export const see = (req, res) => res.send('see');
 
 export default join;
--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,5 +1,13 @@
 export const trending = (req, res) => res.send('Home page Videos');
-export const watch = (req, res) => res.send('Watch');
-export const edit = (req, res) => res.send('Edit');
+export const see = (req, res) => {
+  return res.send(`see #${req.params.id}`);
+};
+export const edit = (req, res) => {
+  return res.send(`Edit #${req.params.id}`);
+};
+
+export const search = (req, res) => res.send('search');
+export const upload = (req, res) => res.send('upload');
+export const deleteVideo = (req, res) => res.send(`deleteVideo #${req.params.id}`);
 
 export default trending;
