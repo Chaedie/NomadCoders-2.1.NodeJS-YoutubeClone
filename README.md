0905b89e04c3069b2acc0c1487ef1b0eca0bd9ab
Chaedie <chaedong.im.dev@gmail.com>
2022-07-01 10:02:53 +0900

    feat : 도메인 별 Router 생성

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 # NomadCoders_2.YotubeClone
+
 x튜브를 클론하며 JS를 배우는 저장소입니다.
 
 기술 스택 : ES6, NodeJS, Express, mongoDB
+
+### Router is...
+
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+/users/edit -> Edit user
+/users/delete -> Delete user
+
+/videos/watch -> Watch Video
+/videos/edit -> Edit Video
+/videos/delete -> Delete Video
+/videos/comments -> Comment on a video
+/videos/comments/delete -> Delete A Comment of a Video
--- src/server.js
+++ src/server.js
@@ -1,35 +1,18 @@
 import express from 'express';
 import morgan from 'morgan';
 
+import globalRouter from './routers/globalRouter';
+import videoRouter from './routers/videoRouter';
+import userRouter from './routers/userRouter';
+
 const PORT = 4000;
 const app = express();
 const logger = morgan('dev');
-
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === '/protected') {
-    return res.send('<h1>Not Allowed</h1>');
-  }
-  console.log('Allowed, you may continue');
-  next();
-};
-
-const handleHome = (req, res) => {
-  // next();
-  return res.send('<h1>I love middleWares</h1>');
-};
-const handleLogin = (req, res) => {
-  return res.send('Login here.');
-};
-const handleProtected = (req, res) => {
-  return res.send('Welcome to the private lounge.');
-};
-
 app.use(logger);
-app.use(privateMiddleware);
-app.get('/', handleHome);
-app.get('/login', handleLogin);
-app.get('/protected', handleProtected);
+
+app.use('/', globalRouter);
+app.use('/users', userRouter);
+app.use('/videos', videoRouter);
 
 const handleListening = () => console.log(`Server listening on port "http://localhost:${PORT}"`);
 app.listen(PORT, handleListening);
