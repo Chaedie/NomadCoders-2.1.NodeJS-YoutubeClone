61ba8e8ccb0350735cd0df60acc2e1fab9d2d601
Chaedie <chaedong.im.dev@gmail.com>
2022-07-08 07:48:53 +0900

    feat : MongoDB 연결 및 스키마 생성

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import './db';
+import './models/Video';
 import express from 'express';
 import morgan from 'morgan';
 
@@ -19,5 +21,5 @@ app.use('/', globalRouter);
 app.use('/users', userRouter);
 app.use('/videos', videoRouter);
 
-const handleListening = () => console.log(`Server listening on port "http://localhost:${PORT}"`);
+const handleListening = () => console.log(`✅ Server listening on port "http://localhost:${PORT}"`);
 app.listen(PORT, handleListening);
