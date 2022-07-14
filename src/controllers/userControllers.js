c1c28dd5affe462cf30b46982e826e0322e401cc
Chaedie <chaedong.im.dev@gmail.com>
2022-07-14 22:36:17 +0900

    feat : 회원가입 구현 (Hasing 미적용)

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,8 +1,22 @@
-export const join = (req, res) => res.send('Join');
+import User from '../models/User';
+
+// /join
+export const getJoin = (req, res) => res.render('join', { pageTitle: 'Join' });
+export const postJoin = async (req, res) => {
+  const { name, email, username, password, location } = req.body;
+  await User.create({
+    name,
+    username,
+    email,
+    password,
+    location,
+  });
+
+  return res.redirect('/login');
+};
+
 export const edit = (req, res) => res.send('Edit');
 export const remove = (req, res) => res.send('Remove');
 export const login = (req, res) => res.send('Login');
 export const logout = (req, res) => res.send('logout');
 export const see = (req, res) => res.send('see');
-
-export default join;
