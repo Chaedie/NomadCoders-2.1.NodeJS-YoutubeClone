ada3d70260667920882fa28f785836ebed7fc3e6
Chaedie <chaedong.im.dev@gmail.com>
2022-07-18 22:42:54 +0900

    add : dotenv 활용해 정보 숨기는 방법 학습

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import session from 'express-session';
+import MongoStore from 'connect-mongo';
 
 import rootRouter from './routers/rootRouter';
 import videoRouter from './routers/videoRouter';
@@ -18,9 +19,14 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   session({
-    secret: 'Hello!',
-    resave: true,
-    saveUninitialized: true,
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 200000,
+    },
+
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
   })
 );
 
